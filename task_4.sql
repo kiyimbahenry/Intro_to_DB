09131b24a25a8f830010ffac5a0cd92f0bb203a8
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-10-19 10:49:16 +0300

    Show tables

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,9 @@
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+-- Prints the full description of the table books from the alx_book_store database
+-- without using DESCRIBE or EXPLAIN
+
+USE alx_book_store;
+
+SHOW CREATE TABLE books;
+
 
 
